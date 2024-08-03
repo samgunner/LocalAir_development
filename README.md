627bd80206e4750ceb707d95726d4a9e5af6a9ae
James Thomas <j.thomas@bristol.ac.uk>
2024-08-03 16:26:21 +0100

    Migrate to PlatformIO

--- README.md
+++ README.md
@@ -4,4 +4,18 @@ This is the first version of the LocalAir microprocesser software, for use on th
 
 This version works, but is grim. A product version is currently under development.
 
-To load this software onto Teensy use the arduino with the Teensy add-on.
+## Setup
+
+This project uses [PlatformIO](https://platformio.org/).
+
+To get started, install either:
+
+- [PlatformIO Core](https://docs.platformio.org/en/latest/core/index.html), a CLI tool, or
+- [PlatformIO IDE](https://docs.platformio.org/en/latest/integration/ide/pioide.html), an integration with VS Code.
+
+## Building
+
+Either:
+
+- Run `pio run --target upload`, if using PlatformIO Core (CLI), or
+- Use the `PlatformIO: Upload` button in VS Code, if using PlatformIO IDE.
---
+++ include/config.h
@@ -0,0 +1,50 @@
+/*
+ * The definitions that go with the LocalAir Software
+ */
+
+// Upload Server details
+#define HTTPS_SERVER "london.localair.uk"
+
+/*
+ * System log file name:
+ */
+#define SYSTEM_LOG_FILE_NAME "LocalAirSystem.log"
+#define ARCHIVE_FOLDER "archive"
+
+// Configure the pins used for the ESP32 connection
+#if defined(TEENSYDUINO)
+#define SPIWIFI SPI    // The SPI port
+#define SPIWIFI_SS 10  // Chip select pin
+#define ESP32_RESETN 5 // Reset pin
+#define SPIWIFI_ACK 7  // a.k.a BUSY or READY pin
+#define ESP32_GPIO0 -1
+#endif
+
+#include <ArduinoHttpClient.h>
+#include <WiFiNINA.h>
+
+#define STREAM false // deciding weather to stream the data or not...
+#define ENCRYPT true // flag to weather to encrypt onto the SD card or not
+
+// and the encryption key information
+#define SPECKKEYSIZE 32
+
+// below a variable that defines the number of bytes is a single line of the encrupted file.
+int line_length = 960;
+
+/* Pollution Sensors */
+#define PMSerial Serial5
+
+/* the sensitivity of the accelerometer interupt */
+#define ACCEL_TRIG_SENSE 0x50
+
+/* Debugging flags */
+#define DEBUG false // this turns on and off debug logging
+#define CHECK_GPS_DATETIME false
+#define AUTO_POWER_OFF true
+#define INACTIVITY_TIMEOUT 5
+#define WIFI_ATTEMPTS 5
+// #define LOG_FILE_NAME_PREFIX "LocalAirData_"
+#define LOG_FILE_NAME_PREFIX DEVICE_ID
+
+/**************************** WiF Status Function ************************************/
