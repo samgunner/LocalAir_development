307173c7747ee27a0a6fbd3c5407adf285a0ba9d
Sam Gunner <sam.gunner@bristol.ac.uk>
2025-04-01 15:14:55 +0100

    changed the upload to point to london server. This does work some of the time, although not as consistantly. The upload generally completes, but then sometimes failes when waiting for a reply from the server, which makes me think that the other end is taking too long to decrypt or something...

--- src/main.cpp
+++ src/main.cpp
@@ -71,10 +71,10 @@ int status = WL_IDLE_STATUS;
 // use the numeric IP instead of the name for the server:
 //IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
 
-#define SERVER "debug.localair.uk"
-#define PATH   "/la_data/LA_999/"
+#define SERVER "london.localair.uk"
+#define PATH   "/la_data/LA_999"
 
-#define FILENAME "workedandfailed.txt"
+#define FILENAME "LAD_231019-222431.txt"
 
 // Initialize the SSL client library
 // with the IP address and port of the server
